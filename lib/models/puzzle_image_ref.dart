86cc6b9378375b47619e3e1bdd25e702afb37b92
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-08-18 15:17:11 +0530

    fix : image addition and fixes

--- lib/models/puzzle_image_ref.dart
+++ lib/models/puzzle_image_ref.dart
@@ -1,17 +1,21 @@
-enum PuzzleImageKind { builtin, file }
+enum PuzzleImageKind { builtin, file, asset }
 
 class PuzzleImageRef {
   const PuzzleImageRef({
     required this.id,
     required this.kind,
     this.filePath,
+    this.assetPath,
     this.builtinIndex,
   });
 
   final String id;
   final PuzzleImageKind kind;
   final String? filePath;
+  final String? assetPath;
   final int? builtinIndex;
+
+  bool get isBundled => kind == PuzzleImageKind.asset;
 
   factory PuzzleImageRef.builtin(int index) {
     return PuzzleImageRef(
@@ -29,10 +33,19 @@ class PuzzleImageRef {
     );
   }
 
+  factory PuzzleImageRef.asset({required String id, required String assetPath}) {
+    return PuzzleImageRef(
+      id: id,
+      kind: PuzzleImageKind.asset,
+      assetPath: assetPath,
+    );
+  }
+
   Map<String, dynamic> toJson() => {
     'id': id,
     'kind': kind.name,
     'filePath': filePath,
+    'assetPath': assetPath,
     'builtinIndex': builtinIndex,
   };
 
@@ -44,6 +57,7 @@ class PuzzleImageRef {
         orElse: () => PuzzleImageKind.builtin,
       ),
       filePath: json['filePath'] as String?,
+      assetPath: json['assetPath'] as String?,
       builtinIndex: json['builtinIndex'] as int?,
     );
   }
